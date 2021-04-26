2f41a92a8a54644f90ed7bbe9f6caec1a4e04f63
krist-all <70210377+krist-all@users.noreply.github.com>
2021-04-26 12:20:19 +0200

    Fixed firestore private key for Heroku

--- database.js
+++ database.js
@@ -1,6 +1,12 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+let serviceAccount;
+
+if(process.env.PRIVATE_KEY){
+   serviceAccount = JSON.parse(process.env.PRIVATE_KEY)
+} else {
+   serviceAccount = require("./serviceAccountKey.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
